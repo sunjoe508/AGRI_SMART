4676bc017089347d91b4996886e5f71512a8b4ea
Joemunga <133594633+sunjoe508@users.noreply.github.com>
2025-07-20 18:10:30 +0300

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,11 +1,33 @@
 import mongoose from 'mongoose';
+import validator from 'validator';
 
 const UserSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    maxlength: 50
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Invalid email format']
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+    select: false // never return password by default
+  }
 }, { timestamps: true });
 
+// Hide password when sending to client
+UserSchema.methods.toJSON = function () {
+  const obj = this.toObject();
+  delete obj.password;
+  return obj;
+};
+
 export default mongoose.model('User', UserSchema);
-
-
